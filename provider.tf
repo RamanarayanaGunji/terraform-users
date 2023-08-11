23a75bd2ad21a371afc0c3378a660772ffe90769
Ramanarayana <gunjirama1999@gmail.com>
2023-08-11 19:32:14 +0530

    vpc created using modules4

--- provider.tf
+++ provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "5.12.0"
     }
   }
+  backend "s3" {
+        bucket = "user-remoted-state"
+        key    =  "user"
+        region = "us-east-1"
+        dynamodb_table = "user-locked"
+      }
 }
 
 provider "aws" {
