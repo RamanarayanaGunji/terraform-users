e5c50a8b5b039276b681dabdb0dd8e95d5c76749
Ramanarayana <gunjirama1999@gmail.com>
2023-08-13 06:56:55 +0530

    tag created

--- main.tf
+++ main.tf
@@ -1,8 +1,12 @@
 module "vpc"{
 source = "../terraform-modules/vpc"
 
-cidr = "10.0.0.0/16"
+cidr = "10.1.0.0/16"
 
 tags = var.tags
+igw-tags = var.igw-tags
+public-subnet-tags = var.public-subnet-tags
+private-subnet-tags = var.private-subnet-tags
+public-route-table-tags = var.public-route-table-tags
 }
 
--- variable.tf
+++ variable.tf
@@ -2,5 +2,32 @@ variable "tags"{
      type = map
      default={
        Name = "timings"
+       environment = "DEV"
+       terraform = "true"
      }
 }
+variable "igw-tags"{
+       type = map
+       default ={
+       Name = "timing-igw"
+       }
+  }
+variable "public-subnet-tags"{
+      type = map
+      default = {
+      Name = "public-subnet-timing"
+      }
+}
+variable "private-subnet-tags"{
+      type = map
+      default = {
+      Name = "private-subnet-timing"
+      }
+}
+variable "public-route-table-tags"{
+      type = map
+      default = {
+      Name = "public-route-table"
+      }
+}
+
